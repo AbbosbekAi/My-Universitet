ce8ff59f2e1deb7301eca8ee0fa860b059bbec3a
AbbosbekAi <qoshmoqboyevabbosbek@gmail.com>
2025-10-21 03:10:20 +0500

    Add files via upload

---
+++ Fakultetlar/Fakultet JS/transport.js
@@ -0,0 +1,20 @@
+const text = "Transport Fakulteti";
+    let i = 0;
+    const speed = 60; // tezligi
+    function typeEffect() {
+      if (i < text.length) {
+        document.getElementById("text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, speed);
+      } else {
+        setTimeout(() => {
+          document.getElementById("text").innerHTML = "";
+          i = 0;
+          typeEffect();
+        }, 15000000); // yozilgandan keyin kutishi
+      }
+
+      setTimeout(animateText, isDeleting ? 100 : 150);
+    }
+
+    typeEffect();
